7ba1f42eaab53ad5d293d84074a64bb20396a4cf
Arthur Markus <info@arthur-markus.com>
2025-07-25 20:03:48 +0200

    Fixed some issues

--- deployment_scripts/terraform/infrastructure/main.tf
+++ deployment_scripts/terraform/infrastructure/main.tf
@@ -55,32 +55,40 @@ resource "aws_security_group" "test_assignment_sg" {
     description = "allow outside access to ec2 instances inside assosiated vpc"
     vpc_id = aws_vpc.test_assignment_vpc.id
 
+    ingress = [
+        {
+            cidr_blocks = [ element(aws_subnet.test_assignment_subnet.*.cidr_block, 0) ]
+            from_port = 9090
+            protocol = "tcp"
+            to_port = 9090
+        },
+        {
+            cidr_blocks = [ element(aws_subnet.test_assignment_subnet.*.cidr_block, 1) ]
+            from_port = 80
+            protocol = "tcp"
+            to_port = 80
+        },
+        {
+            cidr_blocks = [ aws_subnet.test_assignment_subnet.*.cidr_block ]
+            from_port = 22
+            protocol = "tcp"
+            to_port = 22
+        }
+    ]
+
+    egress = [
+        {
+            cidr_blocks = [ "0.0.0.0/0" ]
+            protocol = "-1"
+            from_port = 0
+            to_port = 0
+        }
+    ]
+
     tags = {
       "name" = "test_assignment_sg"
       "project" = "Test Assignment"
     }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "vpc_sg_prometheus_ingress_rule" {
-    ip_protocol = "tcp"
-    security_group_id = aws_security_group.test_assignment_sg.id
-    cidr_ipv4 = element(aws_subnet.test_assignment_subnet.*.cidr_block, 0)
-    from_port = 9090
-    to_port = 9090
-}
-
-resource "aws_vpc_security_group_ingress_rule" "vpc_sg_k3s_ingress_rule" {
-    ip_protocol = "tcp"
-    security_group_id = aws_security_group.test_assignment_sg.id
-    cidr_ipv4 = element(aws_subnet.test_assignment_subnet.*.cidr_block, 1)
-    from_port = 80
-    to_port = 80
-}
-
-resource "aws_vpc_security_group_egress_rule" "vpc_sg_k3s_egress_rule" {
-    ip_protocol = "-1"
-    security_group_id = aws_security_group.test_assignment_sg.id
-    cidr_ipv4 = "0.0.0.0/0"
 }
 
 resource "aws_instance" "prometheus_server" {
@@ -88,6 +96,7 @@ resource "aws_instance" "prometheus_server" {
   instance_type = "t3.micro"
   subnet_id     = element(aws_subnet.test_assignment_subnet.*.id, 0)
   associate_public_ip_address = true
+  key_name = "${var.ec2_key_pair_name}"
 
   tags = {
     Name = "prometheus_server",
@@ -99,6 +108,7 @@ resource "aws_instance" "k3s_server" {
     ami           = "ami-034568121cfdea9c3"
     instance_type = "t3.micro"
     subnet_id     = element(aws_subnet.test_assignment_subnet.*.id, 1)
+    key_name = "${var.ec2_key_pair_name}"
 
     tags = {
         Name = "k3s_server",
@@ -114,6 +124,18 @@ resource "aws_eip" "k3s_server_eip" {
 
     tags = {
         Name = "k3s_server_eip",
+        Project = "Test Assignment"
+    }
+}
+
+resource "aws_instance" "bastion_server" {
+    ami = "ami-034568121cfdea9c3"
+    instance_type = "t3.micro"
+    subnet_id = element(aws_subnet.test_assignment_subnet.*.id, 2)
+    key_name = "${var.ec2_key_pair_name}"
+
+    tags = {
+        Name = "bastion_server",
         Project = "Test Assignment"
     }
 }
--- deployment_scripts/terraform/infrastructure/variables.tf
+++ deployment_scripts/terraform/infrastructure/variables.tf
@@ -1,11 +1,16 @@
 variable "availability_zones" {
   description = "A list containing 3 AZs"
   type        = list(string)
-  default = [ "a", "b" ]
+  default = [ "a", "b", "c" ]
 }
 
 variable "aws_region" {
-    description = "AWS region to deploy to"
-    type = string
-    default = "us-east-1"
+  description = "AWS region to deploy to"
+  type = string
+  default = "us-east-1"
+}
+
+variable "ec2_key_pair_name" {
+  description = "Name of the key pair to assign to the instances"
+  type = string
 }
