c0f7a385e6d2bd3971ee2909f12bc6137fbd5588
Arthur Markus <info@arthur-markus.com>
2025-07-25 17:53:45 +0200

    Fixed some issues

--- deployment_scripts/terraform/infrastructure/main.tf
+++ deployment_scripts/terraform/infrastructure/main.tf
@@ -85,6 +85,14 @@ resource "aws_vpc_security_group_ingress_rule" "vpc_sg_bastion_ingress_rule" {
     to_port = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "vpc_sg_ssm_ingress_rule" {
+    ip_protocol = "tcp"
+    security_group_id = aws_security_group.test_assignment_sg.id
+    cidr_ipv4 = element(aws_subnet.test_assignment_subnet.*.cidr_block, 2)
+    from_port = 443
+    to_port = 443
+}
+
 resource "aws_instance" "prometheus_server" {
   ami           = "ami-034568121cfdea9c3"
   instance_type = "t3.micro"
