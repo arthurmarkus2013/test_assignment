ec26cadd0dcd687cc1cabb0a0c4a5765e86e9df4
Arthur Markus <info@arthur-markus.com>
2025-07-26 10:54:42 +0200

    Fixed some issues

--- deployment_scripts/terraform/infrastructure/main.tf
+++ deployment_scripts/terraform/infrastructure/main.tf
@@ -80,7 +80,7 @@ resource "aws_vpc_security_group_ingress_rule" "web_server_sg_ingress_rule" {
 
 resource "aws_vpc_security_group_ingress_rule" "ssh_sg_ingress_rule" {
     ip_protocol = "tcp"
-    cidr_ipv4 = "10.0.0.0/16"
+    cidr_ipv4 = "0.0.0.0/0"
     security_group_id = aws_security_group.test_assignment_sg.id
     from_port = 22
     to_port = 22    
@@ -114,7 +114,6 @@ resource "aws_instance" "prometheus_server" {
   subnet_id     = element(aws_subnet.test_assignment_subnet.*.id, 0)
   associate_public_ip_address = true
   key_name = "${var.ec2_key_pair_name}"
-  security_groups = [ aws_security_group.test_assignment_sg.name ]
 
   tags = {
     Name = "prometheus_server",
@@ -127,7 +126,6 @@ resource "aws_instance" "k3s_server" {
     instance_type = "t3.micro"
     subnet_id     = element(aws_subnet.test_assignment_subnet.*.id, 1)
     key_name = "${var.ec2_key_pair_name}"
-    security_groups = [ aws_security_group.test_assignment_sg.name ]
 
     tags = {
         Name = "k3s_server",
@@ -153,7 +151,6 @@ resource "aws_instance" "bastion_server" {
     subnet_id = element(aws_subnet.test_assignment_subnet.*.id, 2)
     key_name = "${var.ec2_key_pair_name}"
     associate_public_ip_address = true
-    security_groups = [ aws_security_group.test_assignment_sg.name ]
 
     tags = {
         Name = "bastion_server",
